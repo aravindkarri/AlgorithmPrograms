b8e1f749286cd52b3b6d355d4e32cc843d989204
aravindkarri <aravindkarri98495@gmail.com>
2021-07-10 00:55:42 +0530

    Refactored: Guess a number logic developed

--- src/com/bridgelabz/algorithmprograms/guessnumber/GuessNumber.java
+++ src/com/bridgelabz/algorithmprograms/guessnumber/GuessNumber.java
@@ -5,16 +5,17 @@ public class GuessNumber
 {
 	public static void main(String args[])
 	{
+		boolean isCorrect = false;
 		int minBound =0;
 		int maxBound =1000;
-		System.out.println("Think a number b/w 0 to 100");
-		while(true)
+		System.out.println("Think a number b/w 0 to 1000");
+		while(isCorrect == false)
 		{
 			int mid = (minBound+maxBound)/2;
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("is "+mid+ " is the correct answer: True or False?");
+			System.out.println("is "+mid+ " is the correct answer: true or false?");
 			String isCorrect1 = scanner.next();
-			if(isCorrect1 == "True")
+			if(isCorrect1.equalsIgnoreCase("true"))
 			{
 				System.out.println("The correct answer is "+mid);
 				break;
@@ -23,8 +24,7 @@ public class GuessNumber
 			{
 				System.out.println("is the number greater or less than "+mid + " Enter Greater or Less");
 				String grtOrLess = scanner.next();
-				scanner.close();
-				if (grtOrLess == "Greater")
+				if (grtOrLess.equalsIgnoreCase("greater"))
 				{
 					minBound = mid;
 				}
